836080f6f34bd886f29452a47833e70c514e3a2d
Ayush <achy93858@gmail.com>
2026-08-10 22:20:03 +0545

    Server.js

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/students", require("./routes/studentRoutes"));
 // app.use("/api/test", require("./routes/testRoutes"));
 
 // Test routes
