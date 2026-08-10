b5d2fede241bc700c870c53942fd3efe82fa2b5f
Ayush Chaudhari <achy93858@gmail.com>
2026-08-10 20:50:16 +0545

    Update authControllers

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,7 +2,7 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-// REGISTER
+// REGISTERS
 const register = async (req, res) => {
   try {
     const { name, email, password, role } = req.body;
