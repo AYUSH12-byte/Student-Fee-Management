0123458f79b180c08ad8b2c8fb46061067be0160
Ayush <achy93858@gmail.com>
2026-08-11 18:52:09 +0545

    updates app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,52 +1,45 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "./pages/auth/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          {/* Default */}
+          <Route path="/" element={<Navigate to="/login" replace />} />
 
-        {/* Default route */}
-        <Route
-          path="/"
-          element={<Navigate to="/login" replace />}
-        />
+          {/* Login */}
+          <Route path="/login" element={<Login />} />
 
-        {/* Login */}
-        <Route
-          path="/login"
-          element={<Login />}
-        />
+          {/* Admin Dashboard */}
+          <Route
+            path="/admin/dashboard"
+            element={
+              <ProtectedRoute role="admin">
+                <h1 className="p-8 text-3xl font-bold">Admin Dashboard</h1>
+              </ProtectedRoute>
+            }
+          />
 
-        {/* Admin */}
-        <Route
-          path="/admin/dashboard"
-          element={
-            <h1 className="text-3xl font-bold p-8">
-              Admin Dashboard
-            </h1>
-          }
-        />
+          {/* Student Dashboard */}
+          <Route
+            path="/student/dashboard"
+            element={
+              <ProtectedRoute role="student">
+                <h1 className="p-8 text-3xl font-bold">Student Dashboard</h1>
+              </ProtectedRoute>
+            }
+          />
 
-        {/* Student */}
-        <Route
-          path="/student/dashboard"
-          element={
-            <h1 className="text-3xl font-bold p-8">
-              Student Dashboard
-            </h1>
-          }
-        />
-
-        {/* Invalid route */}
-        <Route
-          path="*"
-          element={<Navigate to="/login" replace />}
-        />
-
-      </Routes>
-    </BrowserRouter>
+          {/* Invalid route */}
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
