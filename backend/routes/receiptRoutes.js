16d117e073dce2907c328aef9ba7ed8aca93b089
Ayush <achy93858@gmail.com>
2026-08-11 10:27:54 +0545

    updates receiptRoutes

--- backend/routes/receiptRoutes.js
+++ backend/routes/receiptRoutes.js
@@ -5,6 +5,7 @@ const {
   getReceipts,
   getReceiptById,
   getReceiptByPayment,
+  downloadReceiptPDF,
 } = require("../controllers/receiptController");
 
 const protect = require("../middleware/authMiddleware");
@@ -12,13 +13,24 @@ const authorize = require("../middleware/roleMiddleware");
 
 const router = express.Router();
 
-// Admin only
+// Generate receipt manually
 router.post("/", protect, authorize("admin"), createReceipt);
 
+// Get all receipts
 router.get("/", protect, authorize("admin"), getReceipts);
 
+// Download receipt PDF
+router.get(
+  "/:id/pdf",
+  protect,
+  authorize("admin", "student"),
+  downloadReceiptPDF,
+);
+
+// Get receipt by ID
 router.get("/:id", protect, authorize("admin"), getReceiptById);
 
+// Get receipt by payment
 router.get(
   "/payment/:paymentId",
   protect,
