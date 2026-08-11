f7060c59c86a1bbbb1c6b9cc82565c329ae2c1cf
Ayush <achy93858@gmail.com>
2026-08-11 18:49:28 +0545

    ProtectedRoute

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,28 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+function ProtectedRoute({ children, role }) {
+  const { user, isAuthenticated } = useAuth();
+
+  // Not logged in
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  // Wrong role
+  if (role && user.role !== role) {
+    if (user.role === "admin") {
+      return <Navigate to="/admin/dashboard" replace />;
+    }
+
+    if (user.role === "student") {
+      return <Navigate to="/student/dashboard" replace />;
+    }
+
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+}
+
+export default ProtectedRoute;
